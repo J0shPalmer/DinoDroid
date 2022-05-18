310c62b6e7eec731fee6c960a327deba02ade22a
Josh <joshua_palmer2911@hotmail.co.uk>
2022-05-18 22:14:10 +0100

    Added test commands

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 const Discord = require("discord.js");
+
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
+
+const prefix = "!";
+
+client.once("ready", () => {
+  console.log("DinoDroid is online");
+});
+
+client.on("messageCreate", (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  const args = message.content.slice(prefix.length).split(/ +/);
+  const command = args.shift().toLowerCase();
+
+  if (command === "hello") {
+    message.channel.send("hallo");
+  } else if (command === "ping") {
+    message.channel.send("pong");
+  }
+});
+
+client.login(
+  "OTc2NTc4MjEwOTc0MjgxODI4.GJQUL8.XYiqXeGiOg4l6jKEeomB-2CdKUm3LOoiRNgwb0"
+);
